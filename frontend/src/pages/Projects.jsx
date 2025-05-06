90d035701b3b010da99c4486c8108959e29f133d
johnvesslyalti <altijohnvessly@gmail.com>
2025-05-06 11:34:36 +0530

    Refactor code structure for improved readability and maintainability

--- frontend/src/pages/Projects.jsx
+++ frontend/src/pages/Projects.jsx
@@ -32,6 +32,28 @@ export default function Projects() {
                     </div>
                 </motion.a>
 
+                <motion.a
+                    href="https://task-master-lime.vercel.app/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    initial={{ opacity: 0, scale: 0.9 }}
+                    whileInView={{ opacity: 1, scale: 1 }}
+                    transition={{ duration: 0.5 }}
+                    viewport={{ once: false }}
+                >
+                    <div className="bg-gray-500 h-50 rounded-xl overflow-hidden relative group cursor-pointer">
+                        <img
+                            src="/images/go-cart.png"
+                            alt="Task Master Project Screenshot"
+                            className="w-full h-full object-cover transition-transform duration-300 group-hover:scale-105"
+                        />
+                        <div className="absolute inset-0 backdrop-blur-xs bg-opacity-60 flex flex-col text-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300 p-5">
+                            <p className="text-gray-500 text-xl font-bold">Task Manager App 🔗</p>
+                            <p className="text-xs">Task Master is a simple and efficient task management app built with React.</p>
+                        </div>
+                    </div>
+                </motion.a>
+
                 {/* Add more projects here using the same pattern */}
             </div>
         </div>
