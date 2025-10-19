8803be3791adcd264691db246cc72b9dc998865f
johnvesslyalti <altijohnvessly@gmail.com>
2025-10-19 09:41:27 +0530

    feat: add my name in spotify-widget

--- src/components/spotify-widget.tsx
+++ src/components/spotify-widget.tsx
@@ -85,8 +85,9 @@ export function SpotifyWidget() {
 
   return (
     <div className="w-50">
-      <div className="flex items-center gap-2 mb-2">
+      <div className="flex items-center justify-between gap-2 mb-2">
         <FaSpotify className="text-green-500 text-sm" />
+        <div className="font-semibold text-xs">Johnvessly Alti</div>
         <span className="font-semibold text-xs">
           {currentTrack?.isPlaying ? "Now Playing" : "Last Listened"}
         </span>
