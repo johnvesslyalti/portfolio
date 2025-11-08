001db8e7421c269dcedeb008f4e53be54e83abd3
johnvesslyalti <altijohnvessly@gmail.com>
2025-11-08 18:43:18 +0530

    fix: small margin fix intro section

--- src/main-components/intro.tsx
+++ src/main-components/intro.tsx
@@ -1,6 +1,9 @@
 export default function Intro() {
   return (
     <section className="border p-5 rounded-lg">
+      <h2 className="text-xl font-semibold mb-3 border-b border-neutral-400 pb-1">
+        About
+      </h2>
       <p className="text-lg leading-relaxed">
         Hi 👋 I’m a <span className="font-semibold">Full Stack Developer</span>{" "}
         passionate about building secure, scalable, and user-friendly web
@@ -12,7 +15,7 @@ export default function Intro() {
         experiences.
       </p>
 
-      <p className="text-lg leading-relaxed mt-2">
+      <p className="text-lg leading-relaxed mt-5">
         I love solving challenging problems and turning ideas into real,
         functional applications. I enjoy building things that make a difference
         and continuously learning along the way.
