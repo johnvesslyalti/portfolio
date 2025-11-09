97c61dccad613de5bec71bdb273247bbbe236fee
johnvesslyalti <altijohnvessly@gmail.com>
2025-11-09 10:35:47 +0530

    feat: add status

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,11 +6,13 @@ import NameSection from "@/main-components/name";
 import ProblemSolving from "@/main-components/problem-solving";
 import Intro from "@/main-components/intro";
 import CustomTweet from "@/main-components/custom-tweet";
+import Status from "@/components/status";
 
 export default function Page() {
   return (
     <div className="min-h-screen flex flex-col gap-10 max-w-2xl mx-auto p-6 text-neutral-900 dark:text-neutral-100 transition-colors">
       <NameSection />
+      <Status />
       <Intro />
       <Skills />
       <Projects />
