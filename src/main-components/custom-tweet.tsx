4180a499cc8145cf97840b38dee208b76f785bd4
johnvesslyalti <altijohnvessly@gmail.com>
2026-01-01 09:22:04 +0530

    feat: change the X tweet

--- src/main-components/custom-tweet.tsx
+++ src/main-components/custom-tweet.tsx
@@ -3,7 +3,7 @@ import { TweetCard } from "@/components/ui/tweet-card";
 export default function CustomTweet() {
   return (
     <div className="flex items-center justify-center border p-5 rounded-lg">
-      <TweetCard className="border-none w-full" id="1992980327288160655" />
+      <TweetCard className="border-none w-full" id="2006572810462961919" />
     </div>
   );
 }
