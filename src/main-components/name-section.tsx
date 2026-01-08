904ccf5cc8da0aa6b214d61313d0db529281bcb3
johnvesslyalti <altijohnvessly@gmail.com>
2026-01-08 14:10:05 +0530

    fix: alignment in name section

--- src/main-components/name-section.tsx
+++ src/main-components/name-section.tsx
@@ -1,21 +1,22 @@
 "use client";
 
 import { AnimatedThemeToggler } from "@/components/ui/animated-theme-toggler";
-import { Dot } from "lucide-react";
 
 export default function NameSection() {
   return (
     <section className="border p-5 rounded-lg">
       {/* Header */}
-      <div className="flex items-center justify-center flex-wrap gap-2">
+      <div className="flex items-center justify-between flex-wrap gap-2">
         <div className="text-3xl sm:text-4xl font-bold tracking-tight">
           Johnvessly Alti
         </div>
 
-        <Dot className="w-4 h-4 opacity-0 md:opacity-100 text-neutral-500 dark:text-neutral-400" />
-        <div>altijohnvessly@gmail.com</div>
-        <Dot className="w-4 h-4 opacity-0 md:opacity-100 text-neutral-500 dark:text-neutral-400" />
-        <AnimatedThemeToggler />
+        <div className="flex items-center gap-4">
+          <div className="text-sm sm:text-base text-neutral-600 dark:text-neutral-400">
+            altijohnvessly@gmail.com
+          </div>
+          <AnimatedThemeToggler />
+        </div>
       </div>
 
 
